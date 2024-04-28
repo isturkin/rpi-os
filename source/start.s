716e2a776f42c6b12928c75ddcab48697f601478
ivanturkin <ivanturkinrussia@gmail.com>
2024-04-28 22:36:19 +0300

    LED indicator flashing example

--- source/start.s
+++ source/start.s
@@ -8,12 +8,29 @@ _start:
 	MOV R1, #1
 	LSL R1, #18 @ shift left by 18 bits
 	STR R1, [R0, #4]
-	
-/* turn on led indicator */
-	MOV R1, #1
+    
+    MOV R1, #1
 	LSL R1, #16
-	STR R1, [R0, #40]
-	
+    
 /* infinite looop */
 loop$:
-	b loop$
+	
+/* turn on led indicator */
+STR R1, [R0, #40]
+    
+MOV R2, #0x3F0000
+wait1$:
+SUB R2, #1
+CMP R2, #0 // changes CPSR register
+BNE wait1$ // branching(B) in case of negative(NE) comparison(!=0)
+
+/* turn off led indicator */
+STR R1, [R0, #28]
+
+MOV R2, #0x3F0000
+wait2$:
+SUB R2, #1
+CMP R2, #0 // changes CPSR register
+BNE wait2$ // wait one more time
+	
+B loop$
